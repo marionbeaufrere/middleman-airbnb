3bfd32a20692ff4eaf063042fd3b411dbe978a10
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-05 15:59:21 +0100

    jolis flats

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["ssaunier", "marionbeaufrere", "thiago9li214", "supershwag"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
